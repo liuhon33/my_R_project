395aee34ca905d002267e625c8ef9a107fd314cd
Hongrui <genius.hongrui.liu@gmail.com>
2024-10-22 22:32:21 -0400

    This is the initial commit. I added the initialize function such that it will return a dataframe containing a population of n individuals, each with a heterouzygous genotype

---
+++ Drift.Utilities.R
@@ -0,0 +1,17 @@
+# comments
+# Names
+# Introduction
+
+# 1a)
+n = 5
+# gen_init is the function that initialize the function,
+# returning the first generation of the population.
+# in this population, every individual is heterouzygous with
+# gene A1 and A2
+gen_init <- function(n){
+    gene1 <- rep("A1", n)
+    gene2 <- rep("A2", n)
+    gen0 <- data.frame(gene1, gene2)
+    # gen0 is the initial generation
+    return(gen0)
+}
